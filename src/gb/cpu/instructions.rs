a15d12e54322a4690f4173eeaa32b09b3e932be0
prasol258@gmail.com <prasol258@gmail.com>
2022-03-13 22:38:04 +0200

    A

--- src/gb/cpu/instructions.rs
+++ src/gb/cpu/instructions.rs
@@ -2,10 +2,6 @@ macro_rules! cpu_instructions {
   ($self: expr, $op: expr) => {
     match($op) {
       0x00 => {},
-      0x01 => {
-        let w = $self.fetch_word();
-        $self.reg.set_bc(w);
-      },
       _ => panic!("Invalid instruction")
     }
   };
