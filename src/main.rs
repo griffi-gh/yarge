f59d1397bf975e2e8c384ed65e9f0db2075d698e
prasol258@gmail.com <prasol258@gmail.com>
2022-03-13 18:15:17 +0200

    Fix main

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,9 @@
 pub mod gb;
 use gb::Gameboy;
-use std::{thread, sync::{Arc, Mutex}};
+use std::sync::{Arc, Mutex};
 
-fn start_emulation_thread(gb: &Arc<Mutex<Gameboy>>) -> thread::JoinHandle<()> {   
-    let gb = Arc::clone(&gb);
-    thread::spawn(move || {
-        loop {
-            let mut gb = gb.lock().unwrap();
-            gb.step();
-            drop(gb);
-        }
-    })
-}
 fn main() {
     let gb = Arc::new(Mutex::new(Gameboy::new()));
-    Gameboy::run_thread(&gb);
-    let thread = start_emulation_thread(&gb);
+    let thread = Gameboy::run_thread(&gb);
     thread.join().unwrap();
 }
