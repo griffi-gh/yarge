a2a3be00cbf78a50d6e5d6ba470fbb5a7bd5f519
prasol258@gmail.com <prasol258@gmail.com>
2022-03-17 23:15:33 +0200

    aaaaaaaaaaaaa

--- src/gb/mmu/cartridge.rs
+++ src/gb/mmu/cartridge.rs
@@ -22,7 +22,7 @@ impl CartridgeNone {
 impl Cartridge for CartridgeNone {
     fn load(&mut self, rom: &[u8]) {
         if rom.len() != 0x8000 {
-            panic!("Invalid size");
+            panic!("Invalid ROM size: {:#X}", rom.len());
         }
         for (place, data) in self.rom.iter_mut().zip(rom.iter()) {
             *place = *data;
