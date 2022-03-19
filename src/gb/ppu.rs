ba5d8abaddec164f74ba1677ac7a0b6b92c5aba4
prasol258@gmail.com <prasol258@gmail.com>
2022-03-19 16:03:01 +0200

    xxx

--- src/gb/ppu.rs
+++ src/gb/ppu.rs
@@ -59,6 +59,45 @@ impl OAMObject {
   }
 }
 
+#[derive(Clone, Copy)]
+pub enum PPUMode {
+  HBlank = 0,
+  VBlank = 1,
+  OAM    = 2,
+  VRAM   = 3,
+}
+impl PPUMode {
+  pub fn from_u8(val: u8) -> Self {
+    #[cfg(not(debug_assertions))]
+    let mut val = val;
+    #[cfg(not(debug_assertions))] {
+      val &= 3;
+    }
+    match val {
+      0 => Self::HBlank,
+      1 => Self::VBlank,
+      2 => Self::OAM,
+      3 => Self::VRAM,
+      #[cfg(not(debug_assertions))]
+      _ => unreachable!(),
+      #[cfg(debug_assertions)]
+      _ => panic!("Invalid mode"),
+    }
+  }
+  pub fn into_u8(&self) -> u8 {
+    *self as u8
+  }
+}
+impl From<u8> for PPUMode {
+  fn from(val: u8) -> Self { Self::from_u8(val) }
+}
+impl Into<u8> for PPUMode {
+  fn into(self) -> u8 { self.into_u8() }
+}
+impl Default for PPUMode {
+  fn default() -> Self { Self::HBlank }
+}
+
 pub struct PPU {
   vram: [u8; 0x2000],
   oam: [OAMObject; 40],
