c51a15c11fe07f70c74086b6be3594aafedf07ff
prasol258@gmail.com <prasol258@gmail.com>
2022-03-21 16:42:08 +0200

    wip

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -13,13 +13,11 @@ const SCALE: u32 = 2;
 
 pub struct GuiState {
   gb: Arc<Mutex<Gameboy>>,
-  error_window_open: bool,
 }
 impl GuiState {
   pub fn new(gb: Arc<Mutex<Gameboy>>) -> Self {
     Self {
-      gb,
-      error_window_open: true,
+      gb
     }
   }
   ///Warning: consumes self!
@@ -32,37 +30,52 @@ impl GuiState {
     });
   }
 }
+fn error_window(ui: &Context, title_text: &str, title_color: Color32, details: &str) {
+  egui::Window::new(RichText::new("Error"))
+    .collapsible(false)
+    .show(ui, |ui| {
+      let title = RichText::new(title_text).color(title_color).size(18.);
+      ui.label(title);
+      ui.collapsing("Details", |ui| {
+        ui.label(details);
+        ui.label("Check console output for more details");
+      });
+    });
+}
 impl Gui for GuiState {
   fn gui(&mut self, ui: &Context) {
     let gb = match self.gb.lock() {
       Ok(gb) => {
-        self.error_window_open = true;
         gb
       },
       Err(err) => {
-        egui::Window::new(RichText::new("Error"))
-          .collapsible(false)
-          .open(&mut self.error_window_open)
-          .show(ui, |ui| {
-            ui.label(
-              RichText::new(format!(
-                "{} has panicked",
-                NAME.unwrap_or("the emulator")
-              ))
-              .color(Color32::from_rgb(0xff, 0x00, 0x00))
-              .size(18.)
-            );
-            ui.collapsing("Details", |ui| {
-              ui.label(format!("{}", err));
-              if let Some(source) = err.source() {
-                ui.label(format!("Caused by: {}", source));
-              }
-              ui.label("Check console output for more details");
-            });
-          });
+        let mut err_info = format!("{}", err);
+        if let Some(source) = err.source() {
+          err_info += format!("\nCaused by: {}", source).as_str();
+        }
+        error_window(
+          &ui,
+          format!(
+            "{} has panicked",
+            NAME.unwrap_or("the emulator")
+          ).as_str(),
+          Color32::RED,
+          err_info.as_str()
+        );
         err.into_inner()
       }
     };
+    /*if let Some(err) = gb.error {
+      error_window(
+        &ui,
+        format!(
+          "{} has suffered an irrecoverable error",
+          NAME.unwrap_or("the emulator")
+        ).as_str(),
+        Color32::YELLOW,
+        "TODO"
+      );
+    }*/
     egui::Window::new(NAME.unwrap_or("debug")).show(ui, |ui| {
       ui.label("Registers");
       ui.horizontal_wrapped(|ui| {
