40744edb08f0421345a86259979b800897621b64
prasol258@gmail.com <prasol258@gmail.com>
2022-03-22 14:42:27 +0200

    zzxzx

--- src/gui/mod.rs
+++ src/gui/mod.rs
@@ -2,7 +2,7 @@ use framework::{
   egui, InitProperties,
   Gui, Dimensions as Dim
 };
-use egui::{Context, RichText, Color32};
+use egui::{Context, RichText, TextStyle, Color32};
 use std::{
   sync::{Mutex, Arc},
   error::Error
@@ -55,7 +55,9 @@ impl Gui for GuiState {
           if ui.button("Exit").clicked() {
             exit = true;
           }
-          //let _ = ui.button("Restart"); TODO
+          ui.add_enabled_ui(false, |ui| {
+            ui.button("Restart").on_disabled_hover_text("WIP");
+          });
         });
         ui.add_space(2.);
       });
@@ -89,13 +91,7 @@ impl Gui for GuiState {
     //error_window(format!("{} crashed", NAME.unwrap_or("emulator")).as_str(), Color32::YELLOW, "TODO", "err_panel");
 
     // MAIN WINDOW
-
-    fn register_view(ui: &mut egui::Ui, name: &str, value: u16) {
-      ui.horizontal(|ui| {
-        ui.monospace(name.to_uppercase());
-        ui.monospace(format!("{:04X}", value));
-      });
-    }
+    let gb_running = gb.running && !crashed;
 
     egui::Window::new(NAME.unwrap_or("debug")).show(ui, |ui| {      
       // Control
@@ -106,28 +102,69 @@ impl Gui for GuiState {
           "Running"
         );
         drop(temp);
-        if let Some(info) = &gb.thread_info {
+        if gb.thread_info.is_some() {
+          let info = gb.thread_info.as_mut().unwrap();
           let elapsed = info.time.elapsed().as_secs_f64();
           ui.label(format!(
             "~{} IPS", ((info.instrs as f64) / elapsed).round() as u64
           ));
+          info.time = std::time::Instant::now();
+          info.instrs = 0;
         }
       }
       // Registers
+      fn register_view(ui: &mut egui::Ui, name: &str, value: u16, allow_edit: bool) -> Option<u16> {
+        let mut ret = None;
+        ui.horizontal(|ui| {
+          ui.monospace(name.to_uppercase());
+          let w = ui.spacing().interact_size.x;
+          if allow_edit {
+            let mut value_str = format!("{:04X}", value).to_string();
+            if ui.add(
+              egui::TextEdit::singleline(&mut value_str)
+                .font(TextStyle::Monospace)
+                .cursor_at_end(true)
+                .desired_width(w)
+                .id_source("regview_".to_string() + name)
+            ).changed() {
+              let x = u16::from_str_radix(value_str.as_str(), 16);
+              if x.is_ok() {
+                ret = Some(x.unwrap());
+              }
+            }
+          } else {
+            ui.monospace(format!("{:04X}", value));
+          }
+        });
+        ret
+      }
       egui::CollapsingHeader::new(
         "Registers"
       ).default_open(true).show(ui, |ui| {
+        let reg = &mut gb.cpu.reg;
         ui.horizontal(|ui| {
-          register_view(ui, "af", gb.cpu.reg.af());
-          register_view(ui, "bc", gb.cpu.reg.bc());
+          if let Some(v) = register_view(ui, "af", reg.af(), !gb_running) {
+            reg.set_af(v);
+          }
+          if let Some(v) = register_view(ui, "bc", reg.bc(), !gb_running) {
+            reg.set_bc(v);
+          }
         });
         ui.horizontal(|ui| {
-          register_view(ui, "de", gb.cpu.reg.de());
-          register_view(ui, "hl", gb.cpu.reg.hl());
+          if let Some(v) = register_view(ui, "de", reg.de(), !gb_running) {
+            reg.set_de(v);
+          }
+          if let Some(v) = register_view(ui, "hl", reg.hl(), !gb_running) {
+            reg.set_hl(v);
+          }
         });
         ui.horizontal(|ui| {
-          register_view(ui, "sp", gb.cpu.reg.sp());
-          register_view(ui, "pc", gb.cpu.reg.pc());
+          if let Some(v) = register_view(ui, "sp", reg.sp(), !gb_running) {
+            reg.set_sp(v);
+          }
+          if let Some(v) = register_view(ui, "pc", reg.pc(), !gb_running) {
+            reg.set_pc(v);
+          }
         });
       });
     });
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+//Hide console in release builds on Windows
+
 pub(crate) use rustish_core as gb;
 use gb::{Gameboy, GameboyBuilder};
 use std::sync::{Arc, Mutex};
