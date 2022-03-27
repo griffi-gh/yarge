cdfce7938294356f7fe57d2ee156a2935c295e81
griffi-gh <prasol258@gmail.com>
2022-03-27 20:44:20 +0300

    WIP PPU

--- lib/rustish_core/src/ppu.rs
+++ lib/rustish_core/src/ppu.rs
@@ -9,12 +9,28 @@ use arraydeque::ArrayDeque;
 const WIDTH: usize = 160;
 const HEIGHT: usize = 144;
 
+struct FifoPixel {
+  color: u8,
+  priority: bool,
+  pal: u8,
+}
+impl Default for FifoPixel {
+  fn default() -> Self {
+    Self {
+      color: 0,
+      priority: false,
+      pal: 0,
+    }
+  }
+}
+
 pub struct PPU {
   pub display: [u8; WIDTH * HEIGHT],
   vram: [u8; 0x2000],
   oam: OAMMemory,
-  _bg_fetcher: BgFetcher,
-  _bg_fifo: ArrayDeque<[u8; 16]>,
+  bg_fetcher: BgFetcher,
+  bg_fifo: ArrayDeque<[FifoPixel; 16]>,
+  obj_fifo: ArrayDeque<[FifoPixel; 16]>,
   lcdc: LCDC,
   pub ly: u8,
 }
@@ -24,8 +40,9 @@ impl PPU {
       display: [0; WIDTH * HEIGHT],
       vram: [0; 0x2000],
       oam: OAMMemory::new(),
-      _bg_fetcher: BgFetcher::new(),
-      _bg_fifo: ArrayDeque::default(),
+      bg_fetcher: BgFetcher::new(),
+      bg_fifo: ArrayDeque::default(),
+      obj_fifo: ArrayDeque::default(),
       lcdc: LCDC::default(),
       ly: 0,
     }
@@ -38,26 +55,21 @@ impl PPU {
     self.lcdc.into_u8()
   }
 
-  #[inline]
-  pub fn read_oam(&self, addr: u16) -> u8 {
+  #[inline] pub fn read_oam(&self, addr: u16) -> u8 {
     self.oam.read_oam(addr - 0xFE00)
   }
-  #[inline]
-  pub fn write_oam(&mut self, addr: u16, value: u8) {
+  #[inline] pub fn write_oam(&mut self, addr: u16, value: u8) {
     self.oam.write_oam(addr - 0xFE00, value);
   }
 
-  #[inline]
-  pub fn read_vram(&self, addr: u16) -> u8 {
+  #[inline] pub fn read_vram(&self, addr: u16) -> u8 {
     self.vram[(addr & 0x1FFF) as usize]
   }
-  #[inline]
-  pub fn write_vram(&mut self, addr: u16, value: u8) {
+  #[inline] pub fn write_vram(&mut self, addr: u16, value: u8) {
     self.vram[(addr & 0x1FFF) as usize] = value;
   }
   
   pub fn tick(&mut self) {
-    
     //TODO ppu.tick()
   }
 }
