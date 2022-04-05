33dd9a136dcba32dde82114d6187882c21a193fe
griffi-gh <prasol258@gmail.com>
2022-04-05 19:15:16 +0000

    Add Setup Script

---
+++ setup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+sudo apt update
+sudo apt install libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev libatk1.0-dev gir1.2-gtk-3.0 
+      
