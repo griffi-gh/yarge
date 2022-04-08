dd9ae193c3aa81e97b049322e8d6aeaa61c348f3
griffi-gh <prasol258@gmail.com>
2022-04-08 16:54:06 +0300

    Update union lib

--- lib/yarge-core/src/cpu/reg/union.rs
+++ lib/yarge-core/src/cpu/reg/union.rs
@@ -8,34 +8,36 @@ pub struct U16Union {
 //MAYBE store two u8?
 
 impl U16Union {
-  pub fn new(value: u16) -> Self { Self { value } }
+  #[inline] pub fn new(value: u16) -> Self {
+    Self { value }
+  }
 
-  #[inline(always)]
-  pub fn set(&mut self, value: u16) {
+  #[inline] pub fn set(&mut self, value: u16) {
     self.value = value;
   }
-  #[inline(always)]
-  pub fn get(&self) -> u16 {
+  #[inline] pub fn get(&self) -> u16 {
     self.value
   }
 
-  #[inline]
-  pub fn set_a(&mut self, value: u8) {
+  #[inline] pub fn set_a(&mut self, value: u8) {
     self.value = (self.value & 0x00FF) | ((value as u16) << 8);
   }
-  #[inline]
-  pub fn set_b(&mut self, value: u8) {
+  #[inline] pub fn set_b(&mut self, value: u8) {
     self.value = (self.value & 0xFF00) | (value as u16);
   }
 
-  #[inline]
-  pub fn get_a(&self) -> u8 {
+  #[inline] pub fn get_a(&self) -> u8 {
     (self.value >> 8) as u8
   }
-  #[inline]
-  pub fn get_b(&self) -> u8 {
+  #[inline] pub fn get_b(&self) -> u8 {
     (self.value & 0xFF) as u8
   }
+}
+impl Into<u16> for U16Union {
+  fn into(self) -> u16 { self.get() }
+}
+impl From<u16> for U16Union {
+  fn from(val: u16) -> Self { Self::new(val) }
 }
 impl Default for U16Union {
   fn default() -> Self { Self::new(0) }
