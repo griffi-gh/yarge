af40008a5eb6722fe82a02971cd4fae391a18d61
griffi-gh <prasol258@gmail.com>
2022-04-20 01:46:58 +0300

    Make write eram self mutable

--- lib/yarge-core/src/mmu/cartridge.rs
+++ lib/yarge-core/src/mmu/cartridge.rs
@@ -15,7 +15,7 @@ pub trait CartridgeImpl {
   fn read_rom(&self, addr: u16) -> u8;
   fn write_rom(&mut self, addr: u16, value: u8) { }
   fn read_eram(&self, addr: u16) -> u8 { 0xff }
-  fn write_eram(&self, addr: u16, value: u8) {}
+  fn write_eram(&mut self, addr: u16, value: u8) {}
   fn save_eram(&self) -> Option<Vec<u8>> { None }
 }
 
