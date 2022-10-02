63024baf6389340580ee21f05771ed223f73d86f
griffi-gh <prasol258@gmail.com>
2022-10-02 18:12:56 +0200

    add more tests

--- yarge-core/src/tests/mod.rs
+++ yarge-core/src/tests/mod.rs
@@ -9,14 +9,20 @@ compile_error!("Enable 'breakpoints' feature to run tests");
 
 macro_rules! define_test {
   ($name: tt, $path: literal, $callback: tt) => {
+    define_test!($name, $path, $callback, (|gb: &mut Gameboy, rom: &[u8]| {
+      gb.init();
+      gb.load_rom(ROM).unwrap();
+      gb.skip_bootrom();
+    }));
+  };
+  ($name: tt, $path: literal, $callback: tt, $setup: tt) => {
     #[test]
     fn $name () {
       use $crate::{Gameboy, YargeError};
       const ROM: &[u8] = include_bytes!(concat!("../../../roms/tests/", $path));
       let mut gb = Gameboy::new();
       gb.init();
-      gb.load_rom(ROM).unwrap();
-      gb.skip_bootrom();
+      $setup(&mut gb, ROM);
       loop {
         match gb.step() {
           Ok(_) => {},
@@ -32,7 +38,7 @@ macro_rules! define_test {
         }
       }
     }
-  }
+  };
 }
 
 macro_rules! define_test_mooneye {
@@ -45,7 +51,36 @@ macro_rules! define_test_mooneye {
       assert_eq!(gb.get_reg_h(), 21);
       assert_eq!(gb.get_reg_l(), 34);
     }));
-  }
+  };
+  ($name: tt, $path: literal, $setup: tt) => {
+    define_test!($name, $path, (|gb: &mut $crate::Gameboy| {
+      assert_eq!(gb.get_reg_b(), 3);
+      assert_eq!(gb.get_reg_c(), 5);
+      assert_eq!(gb.get_reg_d(), 8);
+      assert_eq!(gb.get_reg_e(), 13);
+      assert_eq!(gb.get_reg_h(), 21);
+      assert_eq!(gb.get_reg_l(), 34);
+    }), $setup);
+  };
 }
 
-define_test_mooneye!(mooneye_oam_dma_basic, "mooneye/acceptance/oam_dma/basic.gb");
+define_test_mooneye!(mooneye_acceptance_oam_dma_basic, "mooneye/acceptance/oam_dma/basic.gb");
+define_test_mooneye!(mooneye_acceptance_oam_reg_read, "mooneye/acceptance/oam_dma/reg_read.gb");
+define_test_mooneye!(mooneye_acceptance_oam_sources_gs, "mooneye/acceptance/oam_dma/sources-GS.gb", (|gb: &mut Gameboy, rom: &[u8]| {
+  gb.load_rom_force_mbc(ROM, 0x02).unwrap(); //MBC1+RAM
+  gb.skip_bootrom();
+}));
+
+define_test_mooneye!(mooneye_acceptance_timer_div_write, "mooneye/acceptance/timer/div_write.gb");
+define_test_mooneye!(mooneye_acceptance_timer_rapid_toggle, "mooneye/acceptance/timer/rapid_toggle.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim00_div_trigger, "mooneye/acceptance/timer/tim00_div_trigger.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim00, "mooneye/acceptance/timer/tim00.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim01_div_trigger, "mooneye/acceptance/timer/tim01_div_trigger.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim01, "mooneye/acceptance/timer/tim01.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim10_div_trigger, "mooneye/acceptance/timer/tim10_div_trigger.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim10, "mooneye/acceptance/timer/tim10.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim11_div_trigger, "mooneye/acceptance/timer/tim11_div_trigger.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tim11, "mooneye/acceptance/timer/tim11.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tima_reload, "mooneye/acceptance/timer/tima_reload.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tima_write_reloading, "mooneye/acceptance/timer/tima_write_reloading.gb");
+define_test_mooneye!(mooneye_acceptance_timer_tma_write_reloading, "mooneye/acceptance/timer/tma_write_reloading.gb");
