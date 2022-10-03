5abb191dd8b82ccdaa71ff82958406f8bd0c2990
griffi-gh <prasol258@gmail.com>
2022-10-03 19:12:08 +0200

    update thing

--- yarge-ppu-evt-viewer/index.js
+++ yarge-ppu-evt-viewer/index.js
@@ -1,24 +1,93 @@
 "use strict";
 
-let data;
-
 function loadData(str) {
-  data = str
-    .split('\n')
+  return str
     .replace(/\r/g,'')
+    .split('\n')
     .filter(line => line.startsWith('PPU_EVENT'))
     .map(line => line.split(' ').slice(1))
     .map(line => ({
       type: line[0],
       args: Object.fromEntries(
-        line.split(1).map(x => x.split('=').map(x => x.trim()))
+        line.slice(1).map(x => x.split('=').map(x => x.trim()))
       )
     }));
+}
+
+function drawPoints(data, state) {
+  const canvas = document.getElementById('point-canvas');
+  canvas.width  = parseInt(getComputedStyle(canvas).width);
+  canvas.height = parseInt(getComputedStyle(canvas).height);
+  const scale = canvas.width / (160 * 2);
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'rgba(0,0,0,0)';
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  for (const point of data) {
+    if (!state[point.type]) continue;
+    switch (point.type) {
+      case 'SPR_FETCH_START':
+        ctx.fillStyle = 'rgb(0,0,255)';
+        ctx.globalAlpha = 0.5
+        ctx.fillRect(point.args.lx * scale, point.args.ly * scale, 8 * scale, 3);
+        ctx.globalAlpha = 1;
+        
+        ctx.fillStyle = 'rgb(255,0,0)';
+        ctx.fillRect(point.args.cycles * scale, point.args.ly * scale, 3, 3);
+        break;
+      case 'SPR_FETCH_END':
+        ctx.fillStyle = 'rgb(0,255,0)';
+        ctx.fillRect(point.args.cycles * scale, point.args.ly * scale, 3, 3);
+        break;
+    }
+  }
+  ctx.fillStyle = 'rgba(100,100,255,50)';
+  ctx.fillRect(289 * scale,0,2,canvas.height);
+}
+
+function addToggles(data, cb) {
+  const done = {};
+  const types = [];
+  for(const point of data) {
+    if (done[point.type]) continue;
+    done[point.type] = true;
+    types.push(point.type);
+  }
+
+  const state = {};
+  document.getElementById('toggl-inner').remove();
+  const outer = document.createElement('div');
+  outer.id = 'toggl-inner';
+  for(const type of types) {
+    state[type] = false;
+    const div = document.createElement('div');
+    const inp = document.createElement('input');
+    inp.type = 'checkbox';
+    inp.id = 'input-type-tg-' + type;
+    inp.addEventListener('change', () => {
+      state[type] = !state[type];
+      cb(state);
+    });
+    div.appendChild(inp);
+    const label = document.createElement('label');
+    label.for = inp.id;
+    label.textContent = type;
+    div.appendChild(label);
+    outer.appendChild(div);
+  }
+  document.getElementById('toggl').appendChild(outer);
+  cb(state);
 }
 
 document.getElementById('file-upload').addEventListener('change', event => {
   const file = event.target.files[0];
   const reader = new FileReader();
-  reader.addEventListener('load', loadData);
-  reader.readAsText();
+  reader.addEventListener('load', () => {
+    let data = loadData(reader.result);
+    console.log('data loaded', data);
+    addToggles(data, state => {
+      drawPoints(data, state);
+    });
+    console.log('points drawn');
+  });
+  reader.readAsText(file);
 });
