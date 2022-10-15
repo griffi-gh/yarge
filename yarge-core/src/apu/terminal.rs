7bda8ffd569f5d7da0bebece0e74da3e3c323444
griffi-gh <prasol258@gmail.com>
2022-10-16 01:40:53 +0200

    ridicuosly over optimized crap

--- yarge-core/src/apu/terminal.rs
+++ yarge-core/src/apu/terminal.rs
@@ -1,5 +1,3 @@
-use seq_macro::seq;
-
 #[derive(Default, Clone, Copy)]
 pub struct Terminal {
   pub vin: bool,
@@ -7,14 +5,28 @@ pub struct Terminal {
   pub enabled_channels: (bool, bool, bool, bool),
 }
 impl Terminal {
+  //This is ridicuosly over-optimized but this greatly improves the generated assembly
   pub fn mix_outputs(&self, channels: (f32, f32, f32, f32)) -> f32 {
-    let volume = self.volume as f32 / 7.0;
-    let mut amplitude = 0.;
-    seq!(N in 0..4 {
-      if self.enabled_channels.N {
-        amplitude += channels.N;
-      }
-    });
-    volume * amplitude / 4.0
+    // volume = self.volume / 7.0
+    let volume = {
+      const VOLUME_LUT: [f32; 8] = [
+        0.,
+        1. / 7.,
+        2. / 7.,
+        3. / 7.,
+        4. / 7.,
+        5. / 7.,
+        6. / 7.,
+        1.,
+      ];
+      VOLUME_LUT[(self.volume & 7) as usize]
+    };
+    let amplitude = {
+      f32::from_bits(channels.0.to_bits() * (self.enabled_channels.0 as u32)) +
+      f32::from_bits(channels.1.to_bits() * (self.enabled_channels.1 as u32)) +
+      f32::from_bits(channels.2.to_bits() * (self.enabled_channels.2 as u32)) +
+      f32::from_bits(channels.3.to_bits() * (self.enabled_channels.3 as u32))
+    };
+    volume * amplitude * 0.25
   }
 }
