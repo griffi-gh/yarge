d8e19d950a02a17da22de462f07f668e1295f2d3
griffi-gh <prasol258@gmail.com>
2022-12-01 23:53:17 +0200

    add a warning to `setup.sh`

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+#to be used by the CI scripts only to setup the environment!
+echo '[WARN] to be used by the CI scripts only to setup the environment!'
 sudo apt update
 sudo apt install libgtk-3-dev libxcb-render0-dev libxcb-shape0-dev libxcb-xfixes0-dev libspeechd-dev libxkbcommon-dev libssl-dev libatk1.0-dev gir1.2-gtk-3.0 libasound2-dev
       
