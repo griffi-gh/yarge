61f9608bf7eb9b1d162592977d55a8c462905bdb
griffi-gh <prasol258@gmail.com>
2022-12-06 14:51:50 +0100

    WaveDuty: use u8 instead of [u8; 8]

--- yarge-core/src/apu/terminal.rs
+++ yarge-core/src/apu/terminal.rs
@@ -9,6 +9,17 @@ impl Terminal {
   /// mixes channels together (averages them) with an option to 
   /// disable individual channels (Self.enabled_channels)
   pub fn mix_outputs(&self, channels: (f32, f32, f32, f32)) -> f32 {
+    //Compute sum of all enabled channels
+    let amplitude = {
+      f32::from_bits(channels.0.to_bits() * (self.enabled_channels.0 as u32)) +
+      f32::from_bits(channels.1.to_bits() * (self.enabled_channels.1 as u32)) +
+      f32::from_bits(channels.2.to_bits() * (self.enabled_channels.2 as u32)) +
+      f32::from_bits(channels.3.to_bits() * (self.enabled_channels.3 as u32))
+    };
+    // compute volume:
+    // (self.volume as f32) / 7.
+    // but precomputed
+    // ! volume also does division by 4 to compute average of amplitude instead of sum
     let volume = {
       const VOLUME_LUT: [f32; 8] = [
         0.,
@@ -21,12 +32,6 @@ impl Terminal {
         0.25,
       ];
       VOLUME_LUT[(self.volume & 7) as usize]
-    };
-    let amplitude = {
-      f32::from_bits(channels.0.to_bits() * (self.enabled_channels.0 as u32)) +
-      f32::from_bits(channels.1.to_bits() * (self.enabled_channels.1 as u32)) +
-      f32::from_bits(channels.2.to_bits() * (self.enabled_channels.2 as u32)) +
-      f32::from_bits(channels.3.to_bits() * (self.enabled_channels.3 as u32))
     };
     volume * amplitude
   }
--- yarge-core/src/apu/wave.rs
+++ yarge-core/src/apu/wave.rs
@@ -1,26 +1,39 @@
-pub const WAVE_DUTY_PATTERNS: [[u8; 8]; 4] = [
-  [0, 0, 0, 0, 0, 0, 0, 1], // 12.5%
-  [1, 0, 0, 0, 0, 0, 0, 1], // 25%
-  [1, 0, 0, 0, 0, 1, 1, 1], // 50%
-  [0, 1, 1, 1, 1, 1, 1, 0], // 75%
+const WAVE_DUTY_PATTERNS: [u8; 4] = [
+  0b00000001, // 12.5%
+  0b00000011, // 25%
+  0b00001111, // 50%
+  0b11111100, // 75%
 ];
 
 pub struct WaveDuty {
-  pub pattern: [u8; 8],
-  position: usize,
+  pattern_type: usize,
+  pattern: u8,
+  position: u32,
 }
 impl WaveDuty {
   pub fn new() -> Self {
     Self {
+      pattern_type: 0,
       pattern: WAVE_DUTY_PATTERNS[0],
       position: 0,
     }
   }
   pub fn tick(&mut self) {
     self.position += 1;
-    self.position &= 0x7;
+    self.position &= 7;
   }
   pub fn get_data(&self) -> u8 {
-    self.pattern[self.position]
+    (self.pattern.rotate_left(self.position) & 0x80) >> 7
   }
+  pub fn set_pattern_type(&mut self, pattern_type: usize) {
+    debug_assert!(pattern_type < 4, "Invalid pattern type {}", pattern_type);
+    self.pattern_type = pattern_type;
+    self.pattern = WAVE_DUTY_PATTERNS[pattern_type & 0b11];
+  }
+  pub fn get_pattern_type(&self) -> usize {
+    self.pattern_type
+  }
+}
+impl Default for WaveDuty {
+  fn default() -> Self { Self::new() }
 }
