773401ee60c6e981ecb2ad1ed38fef66025da481
griffi-gh <prasol258@gmail.com>
2022-12-07 02:59:52 +0100

    sqr

--- yarge-core/src/apu/channels/square.rs
+++ yarge-core/src/apu/channels/square.rs
@@ -8,18 +8,20 @@ pub enum SquareWaveChannelType {
 
 pub struct SquareWaveChannel {
   channel_type: SquareWaveChannelType,
-  duty: WaveDuty,
+  wave_duty: WaveDuty,
   wavelength: usize,
   freq_timer: usize,
+  channel_enabled: bool,
 }
 impl SquareWaveChannel {
   pub fn new(channel_type: SquareWaveChannelType) -> Self {
     //TODO provide sensilble defaults?
     Self {
       channel_type,
-      duty: WaveDuty::new(),
+      wave_duty: WaveDuty::new(),
       freq_timer: 8192, //or 0?
       wavelength: 0,
+      channel_enabled: false,
     }
   }
 }
@@ -31,8 +33,16 @@ impl ApuChannel for SquareWaveChannel {
       self.freq_timer -= 1;
       if self.freq_timer == 0 {
         self.freq_timer = 4 * (2048 - self.wavelength);
-        self.duty.tick();
+        self.wave_duty.tick();
       }
     }
+  }
+  fn amplitude(&self) -> f32 {
+    if !self.channel_enabled {
+      return 0.
+    }
+    let data = self.wave_duty.get_data() as f32;
+    //idk why /7.5 - 1 part is needed, I stole it from another emu
+    (data / 7.5) - 1.0 
   }
 }
--- yarge-core/src/apu/traits.rs
+++ yarge-core/src/apu/traits.rs
@@ -1,6 +1,9 @@
 pub trait ApuChannel {
-  fn tick(&mut self); 
+  fn tick(&mut self) {}
   fn tick_length(&mut self) {}
   fn tick_envelope(&mut self) {}
   fn tick_sweep(&mut self) {}
+  fn amplitude(&self) -> f32 { 0. }
+  fn read(&self, mmio_addr: u8) -> u8 { 0 }
+  fn write(&mut self, mmio_addr: u8, value: u8) {}
 }
