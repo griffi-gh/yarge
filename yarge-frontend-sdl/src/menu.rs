c1b7b8389b0e5c90227a75c3f89698f9e744a864
griffi-gh <prasol258@gmail.com>
2022-12-10 21:28:30 +0100

    increase scroll speed

--- yarge-frontend-sdl/src/menu.rs
+++ yarge-frontend-sdl/src/menu.rs
@@ -439,9 +439,9 @@ impl Menu {
       //TODO rewrite!!
       if let Some(cursor_y) = x_cursor_y {
         if cursor_y > res.1 as i32 - (text.char_size(1.).1 as i32 + 2) - MENU_ITEM_HEIGHT as i32 {
-          self.scroll += 1;
+          self.scroll += 3;
         } else if cursor_y < list_start_y_noscroll {
-          self.scroll -= 1;
+          self.scroll -= 3;
         }
       }
 
